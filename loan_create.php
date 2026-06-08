35c749e79cab9ef2e84501aa77a05f07620c3fc5
Juraj Zobka <juraj.zobka@student.ssnd.sk>
2026-06-08 21:19:32 +0200

    Základné zapisovanie do databázy

--- loan_create.php
+++ loan_create.php
@@ -12,6 +12,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'fullName' => $_POST['pouzivatel_id']
     ]);
     $user = $usrStatement->fetch(PDO::FETCH_ASSOC);
-    var_dump($user);
+
+    $specimenInventoryNumber = explode(' ', $_POST['exemplar_id'])[0];
+    $specimenStatement = $pdo->prepare('
+        SELECT
+            id
+        FROM exemplar
+        WHERE inventarne_cislo = :inventoryNumber
+    ');
+    $specimenStatement->execute([
+        'inventoryNumber' => $specimenInventoryNumber
+    ]);
+    $specimen = $specimenStatement->fetch(PDO::FETCH_ASSOC);
+
+    $borrowStatement = $pdo->prepare('
+        BEGIN;
+
+        INSERT INTO vypozicka
+            (pouzivatel_id, exemplar_id, datum_vypozicky, termin_vratenia)
+        VALUES
+            (:userId, :specimenId, :startDate, :returnDate);
+
+        UPDATE exemplar SET
+            stav = \'vypozicany\'
+        WHERE inventarne_cislo = :inventoryNumber;
+
+        COMMIT
+    ');
+    $borrowStatement->execute([
+        'userId' => $user['id'],
+        'specimenId' => $specimen['id'],
+        'startDate' => date('Y-m-d'),
+        'returnDate' => date('Y-m-d', strtotime('+7 days')),
+        'inventoryNumber' => $specimenInventoryNumber
+    ]);
 }
 ?>
