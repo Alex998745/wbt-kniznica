6e1544d4189e39773627124d1987535c086cea0d
Juraj Zobka <juraj.zobka@student.ssnd.sk>
2026-06-08 20:05:28 +0200

    Načítanie používatela

--- load_data.php
+++ load_data.php
@@ -4,18 +4,16 @@ session_start();
 $DB_HOST = "localhost";
 $DB_NAME = "kniznica";
 $DB_USERNAME = "root";
-$DB_PASSWORD = "ssnd";
+$DB_PASSWORD = "password"; //TODO: oficiálne heslo
 
 try {
-
-$pdo = new PDO(
-    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
-    $username,
-    $password
-);
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-    die("Chyba pripojenia k databáe: ".e->getMessage());
+    $pdo = new PDO(
+        "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=utf8mb4",
+        $DB_USERNAME,
+        $DB_PASSWORD
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+    die("Chyba pripojenia k databáze: ".$e->getMessage());
 }
 ?>
--- loan_create.php
+++ loan_create.php
@@ -1,3 +1,17 @@
 <?php
 require 'load_data.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $usrStatement = $pdo->prepare('
+        SELECT
+            id, stav_konta, dovod_zablokovania
+        FROM pouzivatel
+        WHERE CONCAT(meno, \' \', priezvisko) = :fullName
+    ');
+    $usrStatement->execute([
+        'fullName' => $_POST['pouzivatel_id']
+    ]);
+    $user = $usrStatement->fetch(PDO::FETCH_ASSOC);
+    var_dump($user);
+}
 ?>
